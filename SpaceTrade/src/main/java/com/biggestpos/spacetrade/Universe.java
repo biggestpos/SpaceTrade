b067fb85cd23ebdb08a4e1fb66ccd49b815c35e6
robpayne <robpayne@biggestpos.com>
2013-10-29 21:19:14 -0500

    create array of sectors to get the universe started!

--- SpaceTrade/src/main/java/com/biggestpos/spacetrade/Universe.java
+++ SpaceTrade/src/main/java/com/biggestpos/spacetrade/Universe.java
@@ -1,11 +1,29 @@
 package com.biggestpos.spacetrade;
 
+import java.util.Random;
+
 /**
  * Created by robpayne on 10/29/13.
  */
 public class Universe {
 
     int size = 10;
+    long seed = 1492;
+
+    Random myRandom = new Random(seed);
 
     Sector[][][] mySectors = new Sector[size][size][size];
+
+    public Universe(){
+        for(int x = 0; x <= size;x++){
+            for(int y = 0; y <= size;y++){
+                for(int z = 0; z <= size;z++){
+                    Sector aSector = new Sector(x, y, z);
+
+
+                }
+            }
+        }
+    }
+
 }
