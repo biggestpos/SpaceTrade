2c4a76f558dee8ce0b66ea2d85472c5eb476e15f
biggestpos <robpayne@biggestpos.com>
2013-10-30 08:52:39 -0500

    can now scan adjacent sectors

--- SpaceTrade/src/main/java/com/biggestpos/spacetrade/Ship.java
+++ SpaceTrade/src/main/java/com/biggestpos/spacetrade/Ship.java
@@ -4,4 +4,12 @@ package com.biggestpos.spacetrade;
  * Created by robpayne on 10/29/13.
  */
 public class Ship {
+
+    int holds;
+    int speed;
+    int fuel;
+    int food;
+    int fun;
+
+
 }
--- SpaceTrade/src/main/java/com/biggestpos/spacetrade/Universe.java
+++ SpaceTrade/src/main/java/com/biggestpos/spacetrade/Universe.java
@@ -1,5 +1,6 @@
 package com.biggestpos.spacetrade;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -37,7 +38,7 @@ public class Universe {
 
             Sector aSector = mySectors[x][y][z];
 
-            if(aSector.myMarket == null){
+            if(aSector.myMarket == null && aSector.navigability < 0.25){
                 aSector.myMarket = new Market(myRandom.nextInt(10000), myRandom.nextInt(500), myRandom.nextInt(250), myRandom.nextInt(10000));
                 i++;
             }
@@ -45,5 +46,68 @@ public class Universe {
 
         }
     }
+
+    Sector scanSector(int x, int y, int z){
+        return mySectors[x][y][z];
+    }
+
+    ArrayList<Sector> scanAdjacentSectors(int x, int y, int z){
+        ArrayList<Sector> adjacentSectors = new ArrayList<Sector>();
+
+        int xPlus;
+        int xMinus;
+        int yPlus;
+        int yMinus;
+        int zPlus;
+        int zMinus;
+
+        if(x+1 > size){
+            xPlus = 0;
+        } else{
+            xPlus = x+1;
+        }
+
+        if(x-1 < 0){
+            xMinus = size;
+        } else {
+            xMinus = x-1;
+        }
+
+        if(y+1 > size){
+            yPlus = 0;
+        } else{
+            yPlus = y + 1;
+        }
+
+        if(y - 1 < 0){
+            yMinus = size;
+        } else {
+            yMinus = y - 1;
+        }
+
+        if(z + 1 > size){
+            zPlus = 0;
+        } else{
+            zPlus = z + 1;
+        }
+
+        if(z - 1 < 0){
+            zMinus = size;
+        } else{
+            zMinus = z - 1;
+        }
+
+        adjacentSectors.add(mySectors[xPlus][y][z]);
+        adjacentSectors.add(mySectors[xMinus][y][z]);
+
+        adjacentSectors.add(mySectors[x][yPlus][z]);
+        adjacentSectors.add(mySectors[x][yMinus][z]);
+
+        adjacentSectors.add(mySectors[x][y][zPlus]);
+        adjacentSectors.add(mySectors[x][y][zMinus]);
+
+        return adjacentSectors;
+    }
+
 
 }
